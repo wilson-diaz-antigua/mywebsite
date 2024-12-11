e0efc1322ee85226b1e9a409c40ecbff15d36d6d
Wilson <wilson.diant@gmail.com>
2024-12-11 15:43:40 -0500

    Add mobile web app capability metadata in _app.js and refactor component structure

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,15 @@
 import "@/styles/globals.css";
-import { Metadata } from 'next';
 import Layout from "./Layout";
- 
-export const metadata: Metadata = {
-  name: "apple-mobile-web-app-capable",
-  content: "yes",	
-};
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
