596b10891c2cba9c08c2041de40bd31fcce2f763
Wilson <wilson.diant@gmail.com>
2024-12-11 15:12:35 -0500

    Redirect Home page to Landing page using Next.js router

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,12 @@
-import Footer from "./Footer";
-import Header from "./Header";
-import Landing from "./Landing";
-export default function Home() {
-  return (
-    <div className="box-border bg-[#010C15] p-5 text-[#607B96]">
-      <div className="flex min-h-[calc(100vh-40px)] flex-col justify-between rounded-lg border-2 border-[#1E2D3D] bg-[#011627] font-code">
-        <Header setnav={setNav} nav={nav} />
-        <main className={`flex grow flex-col ${nav ? "hidden" : " "} `}>
-          <Landing></Landing>
-        </main>
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-        <Footer />
-      </div>
-    </div>
-  );
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/Landing");
+  }, [router]);
+
+  return null;
 }
