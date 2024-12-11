256fcd35de338c3735a4cc641c6f5628313a3d58
Wilson <wilson.diant@gmail.com>
2024-12-11 15:39:58 -0500

    Add metadata for mobile web app capability in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { Metadata } from 'next';
 import Layout from "./Layout";
+ 
+export const metadata: Metadata = {
+  name: "apple-mobile-web-app-capable",
+  content: "yes",	
+};
+
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
