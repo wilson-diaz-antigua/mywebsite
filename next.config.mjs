cc23f4218267748e1234b74c25e98c45340e956a
Wilson <wilson.diant@gmail.com>
2024-12-25 21:36:04 -0500

    Update URL rewrites in Next.js configuration to point to willdiaz.me for project routes

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,11 @@ const nextConfig = {
     return [
       {
         source: "/project",
-        destination: `${BLOG_URL}/project`,
+        destination: `willdiaz.me/project`,
       },
       {
-        source: "/blog/:path+",
-        destination: `${BLOG_URL}/blog/:path+`,
-      },
-      {
-        source: "/blog-static/_next/:path+",
-        destination: `${BLOG_URL}/blog-static/_next/:path+`,
+        source: "/project/:path+",
+        destination: `willdiaz.me/project/:path+`,
       },
     ];
   },
