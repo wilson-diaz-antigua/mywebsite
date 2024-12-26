49a01e28059da715fdc0c6b1a9e9d568e07f723d
Wilson <wilson.diant@gmail.com>
2024-12-25 21:33:30 -0500

    Add URL rewrites for project and blog routes in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/project",
+        destination: `${BLOG_URL}/project`,
+      },
+      {
+        source: "/blog/:path+",
+        destination: `${BLOG_URL}/blog/:path+`,
+      },
+      {
+        source: "/blog-static/_next/:path+",
+        destination: `${BLOG_URL}/blog-static/_next/:path+`,
+      },
+    ];
+  },
   reactStrictMode: true,
 };
 
