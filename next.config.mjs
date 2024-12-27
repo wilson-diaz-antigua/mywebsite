ccb2196e461e7d2fc82aa11128a3ab8813be66c1
Wilson <wilson.diant@gmail.com>
2024-12-27 14:41:55 -0500

    Add rewrites for project routes to proxy to mta-alerts-tracker

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
       },
     ];
   },
+  async rewrites() {
+    return [
+      {
+        source: "/project/:path*",
+        destination: `https://mta-alerts-tracker.vercel.app/project/:path*`, // Proxy to blog app
+      },
+    ];
+  },
   reactStrictMode: true,
 };
 
