385ce7d1ceeea1057039b9c1a7f12eca4e560d5b
Wilson <wilson.diant@gmail.com>
2024-12-27 18:42:40 -0500

    Fix casing in landing page path for consistency in Header component

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -92,7 +92,7 @@ const Header = (props) => {
         >
           <Link
             className={` ${
-              pathname === "/landing"
+              pathname === "/Landing"
                 ? "bg-[#1e2d3d] text-slate-100 md:border-b-4 md:border-orange-400 md:bg-inherit"
                 : "border-b-[1px] border-[#1E2D3D]"
             } hover:bg-[#1e2d3d]`}
