5177e3cb0b4d86291c9205ff688d141ab9250b52
Wilson <wilson.diant@gmail.com>
2024-12-27 14:49:29 -0500

    Migrate project route rewrites from Next.js configuration to Vercel configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/project/:path*",
-        destination: `https://mta-alerts-tracker.vercel.app/project/:path*`, // Proxy to blog app
-        permanent: true,
-      },
-    ];
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/project/:path*",
-        destination: `https://mta-alerts-tracker.vercel.app/project/:path*`, // Proxy to blog app
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/project/:path*",
+  //       destination: `https://mta-alerts-tracker.vercel.app/project/:path*`, // Proxy to blog app
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/project/:path*",
+  //       destination: `https://mta-alerts-tracker.vercel.app/project/:path*`, // Proxy to blog app
+  //     },
+  //   ];
+  // },
   reactStrictMode: true,
 };
 
