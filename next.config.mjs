259245b58de0eabb5e562801cb0d08aca899cc78
Wilson <wilson.diant@gmail.com>
2024-12-27 12:17:48 -0500

    Update project route source in Next.js configuration to use /project path

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/projects/:path*",
+        source: "/project/:path*",
         destination: `https://mta-alerts-tracker.vercel.app/projects/:path*`, // Proxy to blog app
       },
     ];
