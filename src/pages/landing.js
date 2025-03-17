9e199ca5d11b5bc32f1364c9a8de880a909e4151
Wilson <wilson.diant@gmail.com>
2025-03-17 15:22:31 -0400

    Add type animation to landing page for dynamic content display

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,4 +1,6 @@
 import { useEffect } from "react";
+import { TypeAnimation } from "react-type-animation";
+
 const Landing = () => {
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -20,6 +22,26 @@ const Landing = () => {
               {">"} Full-Stack Developer
             </p>
           </section>
+
+          <div >
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "We produce food for Mice",
+                1000, // wait 1s before replacing "Mice" with "Hamsters"
+                "We produce food for Hamsters",
+                1000,
+                "We produce food for Guinea Pigs",
+                1000,
+                "We produce food for Chinchillas",
+                1000,
+              ]}
+              wrapper="div"
+              speed={50}
+              style={{ fontSize: "2em", display: "inline-block" }}
+              repeat={Infinity}
+            />
+          </div>
 
           {/* github link */}
           <section className="">
