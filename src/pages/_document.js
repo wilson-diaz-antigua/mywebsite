d5e8668e7129944e794cb3c3b27b4630101aa976
Wilson <wilson.diant@gmail.com>
2026-03-28 15:47:42 -0400

    fix: adjust layout and styling in Document and Layout components for improved consistency

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,9 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="min-h-[calc(100dvh)]bg-[#010C15]">
+      <body className="bg-[#010C15]">
         <Main />
         <NextScript />
+        <div className="-z-5 pointer-events-none absolute -bottom-0 -right-0 h-[100lvh] w-[200dvw] opacity-60 md:w-[100dvw]">
+          <Blob />
+        </div>
       </body>
     </Html>
   );
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import Blob from "../../public/blob";
 import Footer from "./footer";
 import Header from "./header";
 
@@ -11,7 +10,7 @@ export default function Layout({ children }) {
     setPageKey((prev) => prev + 1);
   };
   return (
-    <div className="relative box-border bg-[#010C15] p-5 text-[#607B96]">
+    <div className="relative box-border min-h-[calc(100lvh)] bg-[#010C15] p-5 text-[#607B96]">
       <div className="flex min-h-[calc(100dvh)] flex-col justify-between rounded-lg border-2 border-[#1E2D3D] bg-[#011627] font-code">
         <Header
           className=""
@@ -27,9 +26,6 @@ export default function Layout({ children }) {
         </main>
 
         <Footer className="z-10" />
-      </div>
-      <div className="-z-5 pointer-events-none absolute -bottom-0 -right-0 h-[100lvh] w-[200dvw] opacity-60 md:w-[100dvw]">
-        <Blob />
       </div>
     </div>
   );
