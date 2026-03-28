0e54fa6952cf9e488ee8939bdd9acd4dbea07833
Wilson <wilson.diant@gmail.com>
2026-03-28 12:16:03 -0400

    feat: update Blob component structure and replace scene with new Spline file

--- public/blob.jsx
+++ public/blob.jsx
@@ -6,8 +6,8 @@ const Spline = dynamic(() => import("@splinetool/react-spline"), {
 
 export default function Blob() {
   return (
-    <main className="absolute left-0 top-0 -z-10 h-[98svh] w-full">
-      <Spline scene="https://prod.spline.design/T-bh0sxr0UEM4kmk/scene.splinecode" />
-    </main>
+    <div className="absolute left-0 top-0 -z-10 h-[100dvh] w-full">
+      <Spline scene="scene-clean.splinecode" />
+    </div>
   );
 }
