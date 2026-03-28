1e5290b6e5a70dcfb571d2e45f562b2051c4972e
Wilson <wilson.diant@gmail.com>
2026-03-28 14:22:40 -0400

    fix: update minimum height calculation in Layout component to use full 100dvh

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -12,7 +12,7 @@ export default function Layout({ children }) {
   };
   return (
     <div className="relative box-border min-h-[calc(100lvh)] bg-[#010C15] p-5 text-[#607B96]">
-      <div className="flex min-h-[calc(100dvh-40px)] flex-col justify-between rounded-lg border-2 border-[#1E2D3D] bg-[#011627] font-code">
+      <div className="flex min-h-[calc(100dvh)] flex-col justify-between rounded-lg border-2 border-[#1E2D3D] bg-[#011627] font-code">
         <Header
           className=""
           setnav={setNav}
