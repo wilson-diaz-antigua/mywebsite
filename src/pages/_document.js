e57ef14656f2bf00b3f063c7f1363020d39cad27
Wilson <wilson.diant@gmail.com>
2026-03-28 15:34:44 -0400

    fix: add minimum height to body in Document component for better layout consistency

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="bg-[#010C15]">
+      <body className="min-h-[calc(100dvh)]bg-[#010C15]">
         <Main />
         <NextScript />
       </body>
