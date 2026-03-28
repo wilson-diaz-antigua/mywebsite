423e639677f0dd89e7778ea620eed5f828a9100c
Wilson <wilson.diant@gmail.com>
2026-03-28 14:21:03 -0400

    fix: update height in Layout component to use 100lvh for consistency

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -28,7 +28,7 @@ export default function Layout({ children }) {
 
         <Footer className="z-10" />
       </div>
-      <div className="-z-5 pointer-events-none absolute -bottom-0 -right-0 h-[100dvh] w-[200dvw] opacity-60 md:w-[100dvw]">
+      <div className="-z-5 pointer-events-none absolute -bottom-0 -right-0 h-[100lvh] w-[200dvw] opacity-60 md:w-[100dvw]">
         <Blob />
       </div>
     </div>
